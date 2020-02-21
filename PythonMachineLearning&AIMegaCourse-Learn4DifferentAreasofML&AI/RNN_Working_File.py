018703fb5aa3afc2ce459f1efdac64e45ff93575
Acosta Pulido Oscar (RBEI/EBS1.6.5-MX) <aco1ga@bosch.com>
2020-02-21 16:38:33 -0600

    adding changes to models for RNN and Regression

--- PythonMachineLearning&AIMegaCourse-Learn4DifferentAreasofML&AI/RNN_Working_File.py
+++ PythonMachineLearning&AIMegaCourse-Learn4DifferentAreasofML&AI/RNN_Working_File.py
@@ -5,11 +5,15 @@ from sklearn import linear_model, preprocessing
 from sklearn.neighbors import KNeighborsClassifier # classification algorithm
 from sklearn.utils import shuffle
 
-
+# loading data
 data = pd.read_csv("car.data")
 
-# converting non numerical data
+# creating a label encoder object and then use that to encode each column 
+# of our data into integers.
 le = preprocessing.LabelEncoder()
+
+# The method fit_transform() takes a list (each of our columns) and will 
+# return to us an array containing our new values.
 buying = le.fit_transform(list(data["buying"]))
 maint = le.fit_transform(list(data["maint"]))
 door = le.fit_transform(list(data["door"]))
@@ -20,11 +24,29 @@ clas = le.fit_transform(list(data["class"]))
 
 predict = "class"
 
+# we need to recombine our data into a feature list and a label list. 
+# We can use the zip()
 X =  list(zip(buying, maint, door, persons, lug_boot, safety))
 y = list(clas)
 
 # split train testing
+# we will split our data into training and testing data
 x_train, x_test, y_train, y_test = \
     sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 # print(x_train, y_test)
 
+model = KNeighborsClassifier(n_neighbors=9)
+
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
+print(acc)
+
+predicted = model.predict(x_test)
+
+names = ["unacc", "acc", "good", "vgood"]
+
+for x in range(len(predicted)):
+    print("Predicted: ", names[predicted[x]], "Data: ", x_test[x],
+          "Actual: ", names[y_test[x]])
+    n = model.kneighbors([x_test[x]], 9, True)
+    print("Neighbors: ", n)
--- PythonMachineLearning&AIMegaCourse-Learn4DifferentAreasofML&AI/RegressionWorkingFile.py
+++ PythonMachineLearning&AIMegaCourse-Learn4DifferentAreasofML&AI/RegressionWorkingFile.py
@@ -8,36 +8,60 @@ from sklearn import linear_model
 from sklearn.utils import shuffle
 
 
-# read csv file
+# Loading in Our Data
 data = pd.read_csv("student-mat.csv", sep=";")
+
+"""Trimming Our Data
+Since we have so many attributes and not all are relevant we need to select 
+the ones we want to use. We can do this by typing the following."""
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
+""" Separating Our Data
+Now that we've trimmed our data set down we need to separate it into 4 arrays. 
+However, before we can do that we need to define what attribute we are trying 
+to predict. This attribute is known as a label. The other attributes that will 
+determine our label are known as features. Once we've done this we will use 
+numpy to create two arrays. One that contains all of our features and one that 
+contains our labels.
+"""
 predict = "G3"
-
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
+""" 
+After this we need to split our data into testing and training data. We will 
+use 90% of our data to train and the other 10% to test. The reason we do this 
+is so that we do not test our model on data that it has already seen.
+"""
 x_train, x_test, y_train, y_test = \
         sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+
+
 """
+# TRAIN MODEL MULTIPLE TIMES FOR BEST SCORE
 best = 0
 for _ in range(30):
 
     x_train, x_test, y_train, y_test = \
         sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
-    # model definition
-    # commented due pickle model is loaded instead of created it
+    # defining the model which we will be using.
     linear = linear_model.LinearRegression()
-    # model creation
+
+    # train and score our model using the arrays we created
     linear.fit(x_train, y_train)
     acc = linear.score(x_test, y_test)
+
     # accuracy
     print(acc)
 
+    # If the current model has a better score than one we've already trained 
+    # then save it
     if acc > best:
         best = acc
-        # saving pickle model
+
+        # Saving Our Model
+        # To save our model we will write to a new file using pickle.dump().
         with  open("studentmodel.pickle", "wb") as f:
             pickle.dump(linear, f)"""
 
@@ -45,20 +69,24 @@ for _ in range(30):
 pickle_in = open("studentmodel.pickle", "rb")
 linear = pickle.load(pickle_in)
 
-# print accuracy, coeficient, intercept
+# see the constants used to generate the line we can type the following.
+print("-------------------------")
 print("Co: \n", linear.coef_)
 print("Intercept: \n", linear.intercept_)
+print("-------------------------")
 
-# predictions 
+# Gets a list of all predictions
 predictions = linear.predict(x_test)
-
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
 
+
 # attribute retrived from data
-p = "absences"
+# Drawing and plotting model
+p = "absences" # Change this to G1, G2, studytime or absences to see other graphs
 # plot style
 style.use("ggplot")
-plt.scatter(data[p], data["G3"])
+plt.scatter(data[p], data["G1"])
+plt.legend(loc=4)
 plt.xlabel(p); plt.ylabel("Final Grade");
 plt.show()
